1bfa67ce9af4d9c5ff6b674bc7d1d3282c441d5d
Kevin Selvais <kalzone99@gmail.com>
2025-03-06 14:31:25 +0100

    cv update

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -33,7 +33,7 @@ const Hero = () => {
           className="bg-gradient-to-r from-yellow-400 to-red-500 text-white hidden md:inline transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full"
           onClick={() =>
             window.open(
-              "https://drive.google.com/file/d/1FNDmo1im_uDaBDGqppXiZStQ5BNlvF7J/view?usp=sharing",
+              "https://drive.google.com/file/d/1gmOgbphag9C9ndRZH3k70niNm1FwdfI2/view?usp=sharing",
               "_blank"
             )
           }>
